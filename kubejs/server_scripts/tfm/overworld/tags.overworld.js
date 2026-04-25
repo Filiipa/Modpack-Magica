0c90e605b516a5996ff7c0ba33a1fc05f3f91d61
TomPlop <tomdidome@gmail.com>
2026-04-25 18:15:48 -0400

    fix feature

--- kubejs/server_scripts/tfm/overworld/tags.overworld.js
+++ kubejs/server_scripts/tfm/overworld/tags.overworld.js
@@ -4,7 +4,7 @@ function registerTFMOverworldPlacedFeatures(event) {
 
 	// #region Etheria Features
 
-	event.add('tfc:in_biome/surface_structures', 'etheria:etheria/ether_source')
+	event.add('tfc:in_biome/surface_structures', 'tfm:etheria/ether_source')
 
 	// #endregion
 
