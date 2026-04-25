ebd0d6c86bdb6b0e93e9015b708dec14a413f08d
TomPlop <tomdidome@gmail.com>
2026-04-25 13:53:57 -0400

    start integration of features

---
+++ kubejs/server_scripts/tfm/tags.features.js
@@ -0,0 +1,11 @@
+// priority: 0
+
+const registerTFMOFeatures = (event) => {
+
+	// #region Etheria Features
+
+	event.add('tfc:in_biome/erosion', 'tfg:earth/vein/deep_apatite')
+
+	// #endregion
+
+}
