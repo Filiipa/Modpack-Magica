2aad38967f9647f51d20cc2d4f95bb4544f1d125
TomPlop <tomdidome@gmail.com>
2026-04-28 16:35:55 -0400

    add probeJS

--- kubejs/server_scripts/totemic/recipes.js
+++ kubejs/server_scripts/totemic/recipes.js
@@ -8,5 +8,15 @@ function registerTotemicRecipes(event) {
 event.remove({ id: 'totemic:leather_from_hide' })
 event.remove({ id: 'totemic:iron_bells'})
 event.replaceInput({ mod: 'totemic' }, 'totemic:buffalo_hide', 'minecraft:leather')
+event.replaceInput({ id:'totemic:totem_whittling_knife'}, 'minecraft:iron_ingot', 'tfc:metal/knife_blade/copper' )
+
+event.recipes.tfc.shaped('totemic:tipi', [
+		' A ',
+		'ABA',
+		'B B'
+	], {
+		A: '#c:rods/wooden',
+		B: 'minecraft:leather'
+	}).id('tfc:crafting/tipi')
 
 }
